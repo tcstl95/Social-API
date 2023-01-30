2d260fd721a5a1bd947bc635c3ffd2b2ddae7cd5
Tom Carlson <tomcarlson171@gmail.com>
2023-01-30 00:45:16 -0600

    Added more files to MVC

---
+++ controllers/userController.js
@@ -0,0 +1,24 @@
+const User = require('../models/User');
+
+const userController = {
+    // get all users
+    getAllUser(req, res) {
+        User.find({})
+        .populate({
+            path: 'thoughts',
+            select: '-__v'
+        })
+        .populate({
+            path: 'friends',
+            select: '-__v'
+        })
+        .select('-__v')
+        .sort({_id: -1})
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+
+    },
+}
---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,36 @@
+const router = require('express').Router();
+const {
+    getAllThought,
+    getThoughtById,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    deleteReaction
+} = require('../../controllers/thoughtController');
+
+// /api/thoughts
+router
+    .route('/')
+    .get(getAllThought)
+    .post(createThought);
+
+// /api/thoughts/:id
+router
+    .route('/:id')
+    .get(getThoughtById)
+    .put(updateThought)
+    .delete(deleteThought);
+
+// /api/thoughts/:thoughtId/reactions
+router
+    .route('/:thoughtId/reactions')
+    .post(addReaction);
+
+// /api/thoughts/:thoughtId/reactions/:reactionId
+router
+    .route('/:thoughtId/reactions/:reactionId')
+    .delete(deleteReaction);
+
+module.exports = router;
+
