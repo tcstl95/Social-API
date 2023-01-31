084b63a4160f74e76098f62d7d9d180572beeae6
Tom Carlson <tomcarlson171@gmail.com>
2023-01-30 23:51:58 -0600

    Added changes to 404 message

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const routeAPI = require('./api');
 router.use('/api', routeAPI);
 
 router.use((req, res) => {
-    res.status(404).send('<h1> Oh Jesus its a 404 Error!</h1>');
+    res.status(404).send('<h1> Oh Jesus its a 404 Error!</h1>',);
 });
 
 module.exports = router;
